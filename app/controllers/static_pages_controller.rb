f0be26230e404f13f2b170ea0c79b059ad651db7
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-24 00:58:44 +1100

    Add flickr search page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,30 @@
 class StaticPagesController < ApplicationController
   def index
+    flickr = Flickr.new
+
+    @search_params = search_params
+
+    begin
+      user = flickr.people.findByUsername username: search_params[:username]
+      photos = flickr.people.getPhotos user_id: user.id
+      @photo_urls = photos.map { |result| Flickr.url_s(result) }
+    rescue Flickr::FailedResponse
+      begin
+        photos = flickr.people.getPhotos user_id: search_params[:username]
+        @photo_urls = photos.map { |result| Flickr.url_s(result) }
+      rescue Flickr::FailedResponse
+        @photo_urls = {}
+      end
+    end
+  end
+
+  private
+
+  def search_params
+    if params.has_key?(:search)
+      return params.require(:search).permit(:username)
+    end
+
+    {}
   end
 end
